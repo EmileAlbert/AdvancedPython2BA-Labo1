6c087d8c8eca1cb321691337f2bc294244246953
EmileAlbert <ebh.albert@gmail.com>
2016-02-03 14:57:29 +0100

    Ajout de fonction et fichier unittest

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
+from math import *
+
 # Math library
 # Author: Sébastien Combéfis
 # Version: February 2, 2016
+
 
 def fact(n):
     """Computes the factorial of a natural number.
@@ -9,6 +12,11 @@ def fact(n):
     Post: Returns the factorial of 'n'.
     Throws: ValueError if n < 0
     """
+    if n<2:
+        return 1
+    else:
+        return n*fact(n-1)
+
 
 def roots(a, b, c):
     """Computes the roots of the ax^2 + bx + x = 0 polynomial.
@@ -17,9 +25,16 @@ def roots(a, b, c):
     Post: Returns a tuple with zero, one or two elements corresponding
           to the roots of the ax^2 + bx + c polynomial.
     """
-    pass
+    delta = (b*b)-(4*a*c)
+    if delta < 0 :
+        return None
+    elif delta == 0 :
+        return -b/2*a
+    elif delta > 0 :
+        return (-b+sqrt(delta))/2*a, (-b-sqrt(delta))/2*a
 
-def integrate(function, lower, upper):
+
+'''def integrate(function, lower, upper):
     """Approximates the integral of a fonction between two bounds
     
     Pre: 'function' is a valid Python expression with x as a variable,
@@ -27,10 +42,12 @@ def integrate(function, lower, upper):
          'function' continuous and integrable between 'lower‘ and 'upper'.
     Post: Returns an approximation of the integral from 'lower' to 'upper'
           of the specified 'function'.
-    """
-    pass
+    """'''
+
+
+
 
 if __name__ == '__main__':
     print(fact(5))
-    print(roots(1, 0, 1))
-    print(integrate('x ** 2 - 1', -1, 1))
+    print(roots(1, 0,1 ))
+    #print(integrate('x ** 2 - 1', -1, 1))
