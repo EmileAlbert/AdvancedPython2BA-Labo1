96383c79a0b20a6d722a5a344cf5e425ac980f8f
EmileAlbert <ebh.albert@gmail.com>
2016-02-08 14:49:53 +0100

    Fix server.py

--- server.py
+++ server.py
@@ -4,15 +4,29 @@
 # Version: February 3, 2016
 
 import os
-from bottle import route, run
+from bottle import route, run, template, error, post, request, static_file
 import utils
 
 @route('/')
 def home():
     return 'Hello World!'
 
+@route('/hello/<name>')
+def hello(name):
+    return 'Hello {}'.format(name)
+
+@route('/sum/<a>/<b>')
+def sum(a,b):
+    print(type(a))
+    sum = int(a) + int(b)
+    return template('{{a}} + {{b}} = {{s}}', a =a, b=b, s = sum)
+
 @route('/fact/<n>')
 def fact(n) :
-    return utils.fact(n)
+    var = int(n)
+    result = utils.fact(var)
+    return template('La factorielle de {{a}} est {{b}}', a=n,b=result)
+
 
 run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
